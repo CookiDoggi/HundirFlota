2c3611213e7ca096ba9f199ea7a9c5a5467f03c4
cuki <hectorlapaz02@gmail.com>
2022-10-31 13:14:12 +0100

    v1

--- app/src/main/java/com/example/hundirflota/Barco.java
+++ app/src/main/java/com/example/hundirflota/Barco.java
@@ -3,13 +3,16 @@ package com.example.hundirflota;
 public class Barco {
     private int size;
     private OrientacionEnum orientacionActual;
+    private Casilla casillaOrigen;
 
-    public Barco(int size) {
+    public Barco(int size, OrientacionEnum orientacionActual, Casilla casillaOrigen) {
         this.size = size;
+        this.orientacionActual = orientacionActual;
+        this.casillaOrigen = casillaOrigen;
     }
 
     enum OrientacionEnum {
-        norte, sur, este, oeste;
+        //norte, sur, este, oeste;
+        //vertical, horizontal;
     }
 }
-
